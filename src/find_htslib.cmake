960fde4b0cd9a5fe969d1b4c0323d7ffae3dcac2
bistace <bistace@genoscope.cns.fr>
2021-02-09 12:35:42 +0100

    New compilation instructions

---
+++ src/find_htslib.cmake
@@ -0,0 +1,46 @@
+# - Try to find htslib
+# Once done, this will define
+#
+#  htslib_found - system has htslib
+#  htslib_INCLUDE_DIR - the htslib include directories
+#  htslib_LIBRARY - link these to use htslib
+
+set(HTSLIB_SEARCH_DIRS
+        ${HTSLIB_SEARCH_DIRS}
+        $ENV{HTLSIB_ROOT}
+        $ENV{HOME}/local
+        $ENV{HOME}/.local
+        /usr
+        /usr/local
+        )
+
+set(_htslib_ver_path "htslib-${htslib_FIND_VERSION}")
+include(LibFindMacros.cmake)
+
+# Dependencies
+#libfind_package(HTSlib)
+
+# Include dir
+find_path(htslib_INCLUDE_DIR
+        NAMES include/htslib/sam.h
+        PATHS ${HTSLIB_SEARCH_DIRS}
+        HINTS ENV HTSLIB_ROOT
+        )
+
+# Finally the library itself
+find_library(htslib_LIBRARY
+        NAMES hts libhts.a hts.a
+        PATHS ${htslib_INCLUDE_DIR} ${HTSLIB_SEARCH_DIRS}
+        NO_DEFAULT_PATH
+        PATH_SUFFIXES lib lib64 ${_htslib_ver_path}
+        HINTS ENV HTSLIB_ROOT
+        )
+
+if(NOT "${htslib_INCLUDE_DIR}" STREQUAL "htslib_INCLUDE_DIR-NOTFOUND" AND NOT "${htslib_LIBRARY}" STREQUAL "htslib_LIBRARY-NOTFOUND")
+        set(htslib_found "TRUE")
+else()
+        set(htslib_found "FALSE")
+endif()
+message(${htslib_INCLUDE_DIR})
+message(${htslib_LIBRARY})
+message("htslib_found ${htslib_found}\n")
