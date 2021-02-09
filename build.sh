e4ab05f41e7a361cfe41b0d0e1b20c6244354624
bistace <bistace@genoscope.cns.fr>
2021-02-09 12:55:00 +0100

    New compiling instructions

--- build.sh
+++ build.sh
@@ -2,9 +2,39 @@
 set -e
 set -o pipefail
 
+
+help()
+{
+   echo "Hapo-G building script"
+   echo
+   echo "Syntax: bash build.sh -l PATH_TO_HTSLIB"
+   echo "options:"
+   echo "l     Path to htslib. As an example, if samtools is installed in /home/user/samtools, htslib is probably in /home/user/samtools/htslib"
+   echo "h     Print this help."
+   echo
+}
+
+
+while getopts "hl:" flag
+do
+    case "${flag}" in
+        l) 
+            htslib_root=${OPTARG}
+            ;;
+        h) 
+            help
+            exit
+            ;;
+        \?) 
+            echo "Invalid option: -$OPTARG" >&2
+            ;;
+    esac
+done
+
 mkdir build
 cd build
 
+export HTSLIB_ROOT=${htslib_root}
 cmake ../src/
 make
 cd ..
