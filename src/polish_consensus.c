a41b64a272a8a370ea0d3cfcef9a8aac91796ca3
bistace <bistace@genoscope.cns.fr>
2021-10-01 09:57:07 +0200

    Fixed null sequence names when BAM is empty

--- src/polish_consensus.c
+++ src/polish_consensus.c
@@ -166,8 +166,10 @@ int parse_bam(char* bam, char* fa, char* outfa, char *changefile, int silent) {
   for(i = previous_position ; i < ap->len_seq ; i++) 
     select_base(ap, allali, i, s, readname);
   
-  print_seq(s, out);
-  if(!silent && !ap->len_seq) print_step(ap->len_seq, ap->len_seq);
+  if(!ap->len_seq) {
+    print_seq(s, out);
+    if(!silent) print_step(ap->len_seq, ap->len_seq);
+  }
   nb_changes += ap->nb_changes;
   nb_hapB += ap->nbhapB;
   printf("\n\nNumber of reads               : %i\n", tot_read);
