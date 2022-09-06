58ef76ffaaf155da1e8ff91464cfd135889c6131
Benjamin Istace <benjamin.istace@gmail.com>
2022-09-06 14:57:24 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,13 @@
+import distutils.command.build
+
 from setuptools import setup
 from os import path
+
+# Override build command
+class BuildCommand(distutils.command.build.build):
+    def initialize_options(self):
+        distutils.command.build.build.initialize_options(self)
+        self.build_base = 'package_build'
 
 this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, "README.md")) as f:
@@ -32,4 +40,5 @@ setup(
         "Programming Language :: Python :: 3.6",
     ],
     entry_points={"console_scripts" : ["hapog = hapog.cli:main"]},
+    cmdclass={"build": BuildCommand},
 )
