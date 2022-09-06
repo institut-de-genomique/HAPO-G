f817c28318de0d851963d4163c4e4289afa051ef
bistace <benjamin.istace@gmail.com>
2022-09-06 11:38:44 +0200

    Fix conda build

--- conda_files/build.sh
+++ conda_files/build.sh
@@ -4,6 +4,6 @@ export C_INCLUDE_PATH=${PREFIX}/include
 export LIBRARY_PATH=${PREFIX}/lib
 echo $PREFIX
 
-pip install .
+python setup.py install --single-version-externally-managed --record record.txt
 bash build.sh -l $LIBRARY_PATH
 cp -r hapog_build/hapog ${PREFIX}/bin/hapog_bin
