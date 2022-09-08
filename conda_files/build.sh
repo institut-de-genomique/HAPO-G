9cdc75d871cc068fb8b0da42df52e2699cb37bbf
Benjamin Istace <benjamin.istace@gmail.com>
2022-09-08 10:49:05 +0200

    Add ctype.h include for clang compilation

--- conda_files/build.sh
+++ conda_files/build.sh
@@ -2,8 +2,15 @@
 
 export C_INCLUDE_PATH=${PREFIX}/include
 export LIBRARY_PATH=${PREFIX}/lib
-echo $PREFIX
 
-python setup.py install --record record.txt
-bash build.sh -l $LIBRARY_PATH
+python setup.py install --single-version-externally-managed --record=record.txt
+
+mkdir hapog_build && cd hapog_build
+export HTSLIB_ROOT=${LIBRARY_PATH}
+CMAKE_PLATFORM_FLAGS+=(-DCMAKE_TOOLCHAIN_FILE="${RECIPE_DIR}/cross-linux.cmake")
+cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+  ${CMAKE_PLATFORM_FLAGS[@]} \
+  ../src
+make
+cd ..
 cp -r hapog_build/hapog ${PREFIX}/bin/hapog_bin
