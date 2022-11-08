9dc4a6e6888f230e19ce418fa4429d68a1c7e386
Benjamin Istace <benjamin.istace@gmail.com>
2022-11-08 08:19:05 +0100

    Up version

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with open(path.join(this_directory, "README.md")) as f:
 setup(
     name="hapog",
     packages=["hapog"],
-    version="1.3.3",
+    version="1.3.4",
     license="CeCILL",
     description="Haplotype-Aware Polishing of Genomes",
     long_description=long_description,
